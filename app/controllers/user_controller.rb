01cd8002a0996d18c91e3f83a669017d7c1c9dbd
Sebastian Kapunkt <Kindt.sebastian@outlook.com>
2017-07-10 17:02:29 +0200

    Extends User view with game data

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,11 @@
 class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @gamedata = GameData.where(user_id: params[:id])
+
+    @game = Array.new
+    @gamedata.each do |data|
+      @game << Game.find(data.game_id)
+    end
   end
 end
