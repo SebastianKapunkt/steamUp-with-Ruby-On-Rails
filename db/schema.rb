ad0b35d26520ae42a67b20b3f3172f297c5f8db2
Christoph Holtmann <christoph.holtmann@live.de>
2017-07-14 14:17:30 +0200

    Add devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711145420) do
+ActiveRecord::Schema.define(version: 20170714113452) do
 
   create_table "game_stats", force: :cascade do |t|
     t.float "hours"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 20170711145420) do
     t.string "steam_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
   end
 
 end
